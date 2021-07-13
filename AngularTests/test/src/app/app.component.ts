05af0635c782275ac478876b248e64c78e25a30c
Jose Daniel Rodriguez Sanchez <jdrs2483@gmail.com>
2021-07-13 15:03:24 -0600

    Testing Angular - Examples

--- AngularTests/test/src/app/app.component.ts
+++ AngularTests/test/src/app/app.component.ts
@@ -1,18 +1,41 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/forms';
+import { NgModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+@NgModule({
+  imports: [
+             BrowserModule,
+             FormsModule      //<----------make sure you have added this.
+           ]
+})
+
 export class AppComponent {
   stringInterpolation = 'Hola Mundo!';
   propertyBinding = false;
+  serverCreationStatus = 'No server was created!'
+  serverName = 'Test'
 
   constructor(){
     setTimeout(()=>{
       this.propertyBinding = true;
     },2000);
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created'
+  }
+
+  onUpdateServerName(event: any){
+    console.log(event)
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
