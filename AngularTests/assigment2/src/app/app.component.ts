3479acae807f557fba75575f4ca4594c16362090
Jose Daniel Rodriguez Sanchez <jdrs2483@gmail.com>
2021-07-14 19:43:32 -0600

    Testing Angular - Examples

---
+++ AngularTests/assigment2/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'assigment2';
+  username = ''
+  resetButtonDisable = true;
+
+  onUpdateUserName(event: any){
+    this.username = (<HTMLInputElement>event.target).value;
+  }
+
+  checkIfEmpty(event: any){
+    if (this.username != ''){
+      this.resetButtonDisable = false;
+    }
+    else{
+      this.resetButtonDisable = true;
+    }
+  }
+
+  resetValue(){
+    this.username = '';
+    this.resetButtonDisable = true;
+  }
+
+}
