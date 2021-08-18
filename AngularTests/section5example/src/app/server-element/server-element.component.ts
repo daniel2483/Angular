850459093e167d3a548e8adacb0440e24b6cfd48
Jose Daniel Rodriguez Sanchez <jdrs2483@gmail.com>
2021-08-18 11:29:58 -0600

    Angular Course - Section 5 - Encapsulation

--- AngularTests/section5example/src/app/server-element/server-element.component.ts
+++ AngularTests/section5example/src/app/server-element/server-element.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  //encapsulation: ViewEncapsulation.None   // To remove encapsulation
+  //encapsulation: ViewEncapsulation.ShadowDom   // To use the ShadowDom Technology previous was called Native
+  encapsulation: ViewEncapsulation.Emulated   // This is the default encapsulation
 })
 export class ServerElementComponent implements OnInit {
   
